25d032e1bda8417a47bf7a5f8bf00d30dd3b1546
younggue <rladudrb1200@gmail.com>
2017-11-09 13:05:31 +0900

    영규 맵 작업_8

--- app/src/main/java/com/example/minkr/jeonju_all/kindFood/view/KindFoodMap2Activity.java
+++ app/src/main/java/com/example/minkr/jeonju_all/kindFood/view/KindFoodMap2Activity.java
@@ -79,6 +79,7 @@ public class KindFoodMap2Activity extends NMapActivity implements OnMapStateChan
     LinearLayout layoutProgressbar;
 
     int locationType = 0;
+    int locationInt = 0;
     double myLocationX = 0.0;
     double myLocationY = 0.0;
 
@@ -106,8 +107,8 @@ public class KindFoodMap2Activity extends NMapActivity implements OnMapStateChan
 
     public void init(){
         //stopMyLocation();
-        startMyLocation();
         doLocationThing();
+        //startMyLocation();
     }
 
     public void setListener(){
@@ -369,18 +370,22 @@ public class KindFoodMap2Activity extends NMapActivity implements OnMapStateChan
 
         boolean isMyLocationEnabled = nMapLocationManager.enableMyLocation(true);
         if (!isMyLocationEnabled) {
-            Toast.makeText(KindFoodMap2Activity.this, "GPS를 활성화 해주세요.",
-                    Toast.LENGTH_LONG).show();
+            setAlertDialogShow();
 
             return;
         }else{
-            Logger.log("#50 mylocation3 -> "+ myLocationX);
-            String uri ="http://maps.google.com/maps?saddr="+myLocationX+","+myLocationY+"&daddr="+name+"&hl=ko";
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addCategory(Intent.CATEGORY_LAUNCHER );
-            intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
-            startActivity(intent);
+
+            if (myLocationX == 0 || myLocationY == 0){
+                setRoadLocation();
+            }else{
+                Logger.log("#50 mylocation3 -> "+ myLocationX);
+                String uri ="http://maps.google.com/maps?saddr="+myLocationX+","+myLocationY+"&daddr="+name+"&hl=ko";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER );
+                intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                startActivity(intent);
+            }
         }
 
     }
@@ -406,12 +411,23 @@ public class KindFoodMap2Activity extends NMapActivity implements OnMapStateChan
         builder.setNegativeButton("아니오",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(),"위치를 On 하셔야 앱을 사용할 수 있습니다.",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(),"위치를 On 하셔야 현재위치 서비스를 사용할 수 있습니다.",Toast.LENGTH_LONG).show();
                         //finish();
                     }
                 });
         builder.show();
     }
 
+    public void setRoadLocation(){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("길안내 사용방법");
+            builder.setMessage("길안내기능은 현재위치를 받아와야 합니다.\n\n1. 화면우측상단 현재위치 클릭\n2. 다시 화면우측상단 가게위치 클릭\n3. 해당가게의 길안내 다시 클릭");
+            builder.setPositiveButton("확인",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+            builder.show();
+        }
 
 }
